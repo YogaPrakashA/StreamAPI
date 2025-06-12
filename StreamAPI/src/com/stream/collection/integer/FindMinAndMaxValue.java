2695bd0e5d39f96dc320b0afe370593797d54f74
YogaPrakashA <asprakash1208@gmail.com>
2025-06-12 18:38:10 +0530

    FindMinAndMaxValues

---
+++ StreamAPI/src/com/stream/collection/integer/FindMinAndMaxValue.java
@@ -0,0 +1,36 @@
+package com.stream.collection.integer;
+
+import java.util.*;
+
+public class FindMinAndMaxValue {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(71, 2, 53, 14, 55, 6, 97, 83, 9, 10));
+
+        int[] li = new int[]{71, 2, 53, 14, 55, 6, 97, 83, 9, 10};
+
+        // Type-1
+        OptionalInt minInt = Arrays.stream(li).min();
+        OptionalInt maxInt = Arrays.stream(li).min();
+
+        System.out.println("minInt : "+minInt);
+        System.out.println("maxInt : "+maxInt);
+
+        // Type-2
+        OptionalInt minIntReduce = Arrays.stream(li).reduce(Integer::min);
+        OptionalInt maxIntReduce = Arrays.stream(li).reduce(Integer::max);
+
+        System.out.println("minIntReduce : "+minIntReduce);
+        System.out.println("maxIntReduce : "+maxIntReduce);
+
+        // Type-3
+        Arrays.stream(li).min().ifPresent(System.out::println);
+        Arrays.stream(li).max().ifPresent(System.out::println);
+
+        // Type-4
+        Optional<Integer> min = list.stream().min(Comparator.naturalOrder());
+        Optional<Integer> max = list.stream().max(Comparator.naturalOrder());
+
+        System.out.println("min : "+min);
+        System.out.println("max : "+max);
+    }
+}
